1fbf234391a1b7893ef97994ea3082c5e9343a9f
CharlieHendricks <TheMourningDawn@gmail.com>
2015-08-01 23:08:49 -0500

    Got rid of itemRepository, and all the saves. Made the supply link popovers work so they close on the next click. Introduced a bug where you cant right click on a link. Plus the links are not acting like links....but that's unrelated? Added a list of all the images to a post. Still hard coded to one directory though. Updated the single post JSON file a little more. Updated the goggle post a little too.

--- ProjectBlogWebapp/src/main/java/hello/Application.java
+++ ProjectBlogWebapp/src/main/java/hello/Application.java
@@ -1,7 +1,6 @@
 package hello;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +16,6 @@ public class Application implements CommandLineRunner{
 
 	@Autowired
 	private PostRepository repository;
-	@Autowired
-	private SupplyItemRepository itemRepository;
 	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -27,17 +24,10 @@ public class Application implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		repository.deleteAll();
-		itemRepository.deleteAll();
 		
 		ObjectMapper mapper = new ObjectMapper();
 		Post aPost = mapper.readValue(new File("src/main/resources/postJSON.json"), Post.class);
 		
-		System.out.println(aPost);
-		
-		itemRepository.save(new SupplyItem("50mm Costume/Safety Goggles", Arrays.asList(new Link("Adafruit","http://www.adafruit.com/product/1577"),new Link("German Saftey Goggles","http://www.leevalley.com/us/wood/page.aspx?amp;cat=1%2C42207&amp;p=70788"))));
-		itemRepository.save(new SupplyItem("2 x 16 LED NeoPixel Ring", Arrays.asList(new Link("Adafruit","http://www.adafruit.com/product/1463"))));
-		
-
 		// save a couple of Posts
 		repository.save(new Post(1, "A Little Intro", ""));
 		repository.save(aPost);
